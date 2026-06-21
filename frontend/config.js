ac499a4a8c647e0ab47986edbb56b556d8c56e15
MFACHRUDDIN <udinruz@gmail.com>
2026-06-21 07:16:24 +0700

    update config

--- frontend/config.js
+++ frontend/config.js
@@ -9,7 +9,7 @@
 // 2) Setelah backend berhasil online di Railway/Render, ganti
 //    menjadi URL backend kamu, CONTOH:
 //
-//       window.API_BASE_URL = "https://tracker-tugas-backend.up.railway.app";
+//       window.API_BASE_URL = "https://vercel.com/tracker-tugas-copy-productionuprailwayapp/tracker-tugas/5zYpiAUJN6CH6hGEhWgPoLsWPt47";
 //
 //    (Tanpa garis miring "/" di akhir URL)
 // ============================================================
